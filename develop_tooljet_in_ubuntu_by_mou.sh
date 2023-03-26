7af9cb5638f3e9ed0c3e99ff60a4465604307ff2
moushengkoo <10402885@qq.com>
2023-03-26 23:04:34 +0800

    添加一键二次开发脚本

---
+++ develop_tooljet_in_ubuntu_by_mou.sh
@@ -0,0 +1,72 @@
+#/bin/bash
+
+# 本脚本用于Ubuntu下一键二次开发tooljet
+# 原理就是使用docker镜像生成客户端和服务端及数据库
+# 无需在Ubuntu系统中安装node、Python、数据库等
+# 但请确保Ubuntu已安装docker
+# 如不想开启自带数据库，可注释下面的两块代码段,最后docker编译命令需要权限，记得启动加上sudo
+
+# pull镜像
+docker pull node:18.3.0-buster
+docker pull postgrest/postgrest:v10.1.1.20221215
+docker pull postgres:13
+
+# 删除原docker
+docker-compose down
+
+# 复制环境变量文件
+cp .env.example .env
+cp .env.example .env.test
+
+SECRET_KEY_32=$(openssl rand -hex 32)
+SECRET_KEY_64=$(openssl rand -hex 64)
+
+envFile='.env'
+sed -i "s/^LOCKBOX_MASTER_KEY=.*/LOCKBOX_MASTER_KEY=$SECRET_KEY_32/g" $envFile
+sed -i "s/^SECRET_KEY_BASE=.*/SECRET_KEY_BASE=$SECRET_KEY_64\nNODE_ENV=development/g" $envFile
+sed -i "s/^SERVER_HOST=.*/SERVER_HOST=localhost/g" $envFile
+sed -i "s/^ORM_LOGGING=.*/ORM_LOGGING=all/g" $envFile
+sed -i "s/^PG_DB=.*/PG_DB=tooljet_development/g" $envFile
+sed -i "s/^PG_USER=.*/PG_USER=postgres/g" $envFile
+sed -i "s/^PG_HOST=.*/PG_HOST=postgres\nPG_PORT=5432/g" $envFile
+sed -i "s/^PG_PASS=.*/PG_PASS=postgres/g" $envFile
+# 开启自带数据库，不想开启则注释以下内容
+sed -i "s/^ENABLE_TOOLJET_DB=.*/ENABLE_TOOLJET_DB=true/g" $envFile
+sed -i "s/^TOOLJET_DB=.*/TOOLJET_DB=tooljet_db/g" $envFile
+sed -i "s/^TOOLJET_DB_USER=.*/TOOLJET_DB_USER=postgres/g" $envFile
+sed -i "s/^TOOLJET_DB_HOST=.*/TOOLJET_DB_HOST=postgres\nTOOLJET_DB_PORT=5432/g" $envFile
+sed -i "s/^TOOLJET_DB_PASS=.*/TOOLJET_DB_PASS=postgres/g" $envFile
+sed -i "s/^PGRST_HOST=.*/PGRST_HOST=postgrest:3000\nPGRST_LOG_LEVEL=info\nPGRST_DB_URI=postgres:\/\/postgres:postgres@postgres:5432\/tooljet_db/g" $envFile
+sed -i "s/^PGRST_JWT_SECRET=.*/PGRST_JWT_SECRET=$SECRET_KEY_32/g" $envFile
+# end
+
+
+
+envFile='.env.test'
+#写入.env.text环境变量
+sed -i "s/^LOCKBOX_MASTER_KEY=.*/LOCKBOX_MASTER_KEY=$SECRET_KEY_32/g" $envFile
+sed -i "s/^SECRET_KEY_BASE=.*/SECRET_KEY_BASE=$SECRET_KEY_64\nNODE_ENV=test/g" $envFile
+sed -i "s/^SERVER_HOST=.*/SERVER_HOST=localhost/g" $envFile
+sed -i "s/^ORM_LOGGING=.*/ORM_LOGGING=error/g" $envFile
+sed -i "s/^PG_DB=.*/PG_DB=tooljet_development/g" $envFile
+sed -i "s/^PG_USER=.*/PG_USER=postgres/g" $envFile
+sed -i "s/^PG_HOST=.*/PG_HOST=postgres\nPG_PORT=5432/g" $envFile
+sed -i "s/^PG_PASS=.*/PG_PASS=postgres/g" $envFile
+
+# 开启自带数据库，不想开启则注释以下内容
+sed -i "s/^ENABLE_TOOLJET_DB=.*/ENABLE_TOOLJET_DB=true/g" $envFile
+sed -i "s/^TOOLJET_DB=.*/TOOLJET_DB=tooljet_db/g" $envFile
+sed -i "s/^TOOLJET_DB_USER=.*/TOOLJET_DB_USER=postgres/g" $envFile
+sed -i "s/^TOOLJET_DB_HOST=.*/TOOLJET_DB_HOST=postgres\nTOOLJET_DB_PORT=5432/g" $envFile
+sed -i "s/^TOOLJET_DB_PASS=.*/TOOLJET_DB_PASS=postgres/g" $envFile
+sed -i "s/^PGRST_HOST=.*/PGRST_HOST=postgrest:3000\nPGRST_LOG_LEVEL=info\nPGRST_DB_URI=postgres:\/\/postgres:postgres@postgres:5432\/tooljet_db/g" $envFile
+sed -i "s/^PGRST_JWT_SECRET=.*/PGRST_JWT_SECRET=$SECRET_KEY_32/g" $envFile
+# end
+
+docker-compose build
+docker-compose run --rm  plugins npm run build:plugins
+docker-compose up
+
+
+
+
--- rebuildDocker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-docker-compose down
-docker-compose build
-docker-compose run --rm plugins npm run build:plugins
-docker-compose up
